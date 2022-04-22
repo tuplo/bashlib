63bad142b4adbd84e6fb68c55d7d84c137c83b68
Rui Costa <ruicosta042@gmail.com>
2022-04-22 15:24:00 +0100

    feat: install script

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # @tuplo/bashlib
+
+Collection of `bash` utility functions.
+
+## Install
+
+```bash
+wget -qO- https://raw.githubusercontent.com/tuplo/bashlib/install.sh | bash
+```
+
+This will create a directory named `bashlib` on the path it was called from.
+
+## Usage
+
+When the client scripts are located on the same directory as `bashlib`:
+
+```bash
+
+source ./@tuplo/bashlib/.sh
+
+```
+
+## License
+
+MIT
---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+{
+	url="https://github.com/tuplo/bashlib//tarball/main"
+	dest="${PWD}/@tuplo/bashlib"
+
+	if [[ -n "$(which curl)" ]]; then
+		download="curl -sSL ${url}"
+	elif [[ -n "$(which wget)" ]]; then
+		download="wget -q ${url} -O -"
+	else
+		echo "Neither curl nor wget found"
+		exit 1
+	fi
+
+	rm -rf "${dest:?}"
+	mkdir -p "${dest:?}"
+
+	if ! ${download} | tar xz -C "${dest}" --strip-components=2; then
+		echo "Failed to download @tuplo/bashlib"
+		exit 1
+	fi
+
+	echo "@tuplo/bashlib installed at ${dest}"
+}
